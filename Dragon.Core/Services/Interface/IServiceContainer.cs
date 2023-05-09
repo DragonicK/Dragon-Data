738e95a554df9e42f9d07b528a11738ed9a8ddd5
Julio Sperandio <juliosperandio@hotmail.com>
2023-05-08 22:41:54 -0300

    Added services.

---
+++ Dragon.Core/Services/Interface/IServiceContainer.cs
@@ -0,0 +1,9 @@
+﻿namespace Dragon.Core.Services;
+
+public interface IServiceContainer {
+    int Count { get; }
+    void Add(Type type, IService instance);
+    IService[] ToArray();
+    IService this[Type type] { get; }
+    IUpdatableService[] GetUpdatableServices();
+}
---
+++ Dragon.Core/Services/ServiceInjector.cs
@@ -0,0 +1,60 @@
+﻿using System.Reflection;
+
+namespace Dragon.Core.Services;
+
+public sealed class ServiceInjector : IServiceInjector {
+
+    private readonly IServiceContainer _container;
+
+    public ServiceInjector(IServiceContainer container) {
+        _container = container;
+    }
+
+    public void Inject(object target) {
+        InjectObject(target);
+        InjectArray(target);
+        InjectContainer(target); 
+        InjectInjector(target);
+    }
+
+    private void InjectObject(object target) {
+        var targetType = target.GetType();
+        var properties = targetType.GetRuntimeProperties()
+            .Where(p => p.PropertyType.GetInterface(nameof(IService)) is not null);
+
+        var values = properties.Select(p => (p.Name, p.PropertyType)).ToArray();
+
+        foreach (var (name, propertyType) in values) {
+            var property = properties.Where(p => p.Name == name).First();
+
+            property?.SetValue(target, _container[propertyType]);
+        }
+    }
+
+    private void InjectArray(object target) {
+        var targetType = target.GetType();
+        var properties = targetType.GetRuntimeProperties()
+              .Where(p => p.PropertyType == typeof(IService[]))
+              .FirstOrDefault();
+
+        properties?.SetValue(target, _container.ToArray());
+    }
+
+    private void InjectContainer(object target) {
+        var targetType = target.GetType();
+        var properties = targetType.GetRuntimeProperties()
+              .Where(p => p.PropertyType == typeof(IServiceContainer))
+              .FirstOrDefault();
+
+        properties?.SetValue(target, _container);
+    }
+
+    private void InjectInjector(object target) {
+        var targetType = target.GetType();
+        var properties = targetType.GetRuntimeProperties()
+              .Where(p => p.PropertyType == typeof(IServiceInjector))
+              .FirstOrDefault();
+
+        properties?.SetValue(target, this);
+    }
+}
